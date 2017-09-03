4c6e675f124287e78d6578f849a35ff8e0eccd2e
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-03 11:54:03 +0100

    Updates with test for loops

--- MyFirstApp.java
+++ MyFirstApp.java
@@ -3,6 +3,15 @@ public class MyFirstApp {
   public static void main (String[] args) {
     System.out.println("I Rule!");
     System.out.println("The World");
-  }
 
+    int[] testArray = {10,20,30};
+
+    for (int x = 0; x < testArray.length; x = x + 1) {
+      testArray[x] = testArray[x] + 1;
+    }
+
+    for (int number : testArray) {
+      System.out.println(number);
+    }
+  }
 }
---
+++ test2.java
@@ -0,0 +1,33 @@
+int x = 3;
+
+String name = "Dirk";
+
+int x = x * 17;
+
+System.out.print("x is " + x);
+int double d = Math.random();
+
+
+while (x > 12) {
+  x = x -1;
+}
+
+
+for (int x = 0; x < 10; x = x + 1) {
+  System.out.print("x is now " + x);
+}
+
+for (int x = 0; x < 10; x++) {
+  System.out.print("x is now " + x);
+}
+
+for (int number : testArray) {
+  number + 1;
+}
+
+int[] testArray = {10,20,30}
+
+for (int x = 0; x < testArray.length; x = x + 1) {
+  testArray[x] = x + 1
+  System.out.print("x is now " + x);
+}
