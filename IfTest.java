7b369f7adbdab19d738c6ed8f75c6d1d00e3bd7c
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-03 13:28:37 +0100

    Updates test conditional statement

--- IfTest.java
+++ IfTest.java
@@ -3,6 +3,8 @@ class IfTest {
     int x = 3;
     if (x == 3) {
       System.out.println("x must be 3");
+    } else {
+      System.out.println("x is NOT 3");
     }
       System.out.println("This runs no matter what");
   }
