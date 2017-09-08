59ef8a61d423c8c4c980939406fa676ba6a261e7
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-08 12:06:42 +0100

    Updates solutions

--- ch8/InterfacesExercise.java
+++ ch8/InterfacesExercise.java
@@ -3,23 +3,23 @@ public class Click {}
   public class Clack extends Click {}
 
 
-// 2.
-public abstract class Top {}
-  public class Tip extends Top {}
+    // 2.
+    public abstract class Top {}
+      public class Tip extends Top {}
 
 
-// 3.
-public abstract class Fee {}
-  public abstract class Fi extends fee {}
+        // 3.
+        public abstract class Fee {}
+          public abstract class Fi extends Fee {}
 
 
-// 4.
-public interface Foo {}
-  public class Bar implements Foo {}
-    public class Baz extends Bar {}
+            // 4.
+            public interface Foo {}
+              public class Bar implements Foo {}
+                public class Baz extends Bar {}
 
-// 5.
-public interface Zeta {}
-  public class Aplpha implements Zeta {}
-    public class Delta extends Alpha {}
-      public interface Beta implements Delta {}
+                  // 5.
+                  public interface Zeta {}
+                    public class Aplpha implements Zeta {}
+                      public interface Beta {}
+                        public class Delta extends Alpha implements Beta {}
