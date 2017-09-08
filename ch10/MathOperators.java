6b1e90af8eb0dd19f11be73f06a2b0e235ea79ac
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-08 15:39:31 +0100

    Adds in example of static classes

---
+++ ch10/MathOperators.java
@@ -0,0 +1,3 @@
+int x = Math.round(42.2);
+int y = Math.round(56,12);
+int z = Math.round(-343);
---
+++ ch10/StaticDuck.java
@@ -0,0 +1,17 @@
+public class Duck {
+
+  private int size;
+  private static int duckCount = 0;
+  //static duckCount initialized only when the class is first loaded, not each time a new class is made 
+
+  public Duck() {
+    duckCount++;
+  }
+
+  public void setSize(int s) {
+    size = 2;
+  }
+  public int getSize() {
+    return size;
+  }
+}
