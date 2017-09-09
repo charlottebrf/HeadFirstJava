53a4a9fc22f11122f1c701d5af1c5729ecda2101
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-09 12:44:37 +0100

    Updates solution with created exception classes

--- ch11/MoreExceptions.java
+++ ch11/MoreExceptions.java
@@ -5,5 +5,27 @@ try {
 } catch (LingerieException lex) {
   //recovery
 } catch (ClothingException cex) {
-  //recovery 
+  //recovery
 }
+
+//extending multiple classes
+
+//
+// the smallest exception goes first
+try {
+  x.doRiskyThing();
+} catch (BoinkEx e){
+  //recovery  //recovery
+} catch (FooEx c) {
+  // Foo catches Baz & Biff as they are extensions of the Foo 
+  //recovery
+}  catch(BazEx a)  {
+  //recovery
+}
+// the biggest exception goes last
+
+class BazEx extends Exception {}
+class FooEx extends BazEx {}
+class BarEx extends FooEx {}
+class BiffEx extends FooEx {}
+class BoinkEx extends BiffEx {}
